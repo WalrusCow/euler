44cba5dae444fe8147f00d7de5abb8802ae6e805
WalrusCow <willgmcd@gmail.com>
2017-07-09 12:52:09 -0700

    New sieve class

--- euler.py
+++ euler.py
@@ -3,6 +3,7 @@ euler.py
 Random utilities written during euler questions.
 '''
 
+
 def permuteInts(L):
     ''' Permute a list of digits into integers. '''
     previous = set()
@@ -16,6 +17,7 @@ def permuteInts(L):
                     previous.add(perm)
     return _permute(L, 0)
 
+
 def digitize(n):
     '''
     Return a list of the digits of an integer.
@@ -26,6 +28,7 @@ def digitize(n):
         l.append(n % 10)
         n //= 10
     return l
+
 
 def undigitize(li):
     '''
@@ -38,6 +41,7 @@ def undigitize(li):
         ans += d * m
         m *= 10
     return ans
+
 
 def slowPrimes(n):
     ''' Return: (primes, sieve) for primes under n
@@ -55,6 +59,7 @@ def slowPrimes(n):
                 primeSieve[j] = False
     return primeList, primeSieve
 
+
 def primes(n):
     ''' Return: (primes, sieve) for primes under n
     The sieve has no even numbers in it, and begins at 3
@@ -69,6 +74,7 @@ def primes(n):
                 primeSieve[j] = False
     return primeList, primeSieve
 
+
 def rotate(n):
     ''' Rotate the number n.
     e.g. 123 -> 123, 231, 312
@@ -77,6 +83,7 @@ def rotate(n):
     digitList = digitize(n)
     for i in range(len(digitList)):
         yield undigitize(digitList[i:] + digitList[:i])
+
 
 def isPalindrome(li):
     ''' Determine if a list is a palindrome. '''
@@ -87,3 +94,48 @@ def isPalindrome(li):
         if li[i] != li[l - 1 - i]:
             return False
     return True
+
+
+class PrimeSieve():
+    def __init__(self, sieve_size):
+        self._sieve = [True] * (sieve_size // 2)
+        self._sieve[0] = False
+        self.primes = [2]
+        for idx in range(len(self._sieve)):
+            if not self._sieve[idx]:
+                continue
+            n = idx * 2 + 1
+            self.primes.append(n)
+            not_prime = n + n + n
+            while not_prime < sieve_size:
+                self._sieve[not_prime // 2] = False
+                not_prime += n + n
+        print('Constructed sieve size {}'.format(sieve_size))
+
+
+    def is_prime(self, n):
+        if n % 2 == 0:
+            return n == 2
+        if n <= 1:
+            return False
+        if n < len(self):
+            return self._sieve[n // 2]
+        largest_prime_to_check = math.ceil(math.sqrt(n))
+        if largest_prime_to_check > len(self):
+            raise ValueError(
+                'Sieve length {} too small to check primality of {}'.format(
+                    len(self),
+                    n,
+                )
+            )
+        for p in self.primes:
+            if p > largest_prime_to_check:
+                return True
+            if n % p == 0:
+                return False
+        return False
+
+
+
+    def __len__(self):
+        return len(self._sieve) * 2
